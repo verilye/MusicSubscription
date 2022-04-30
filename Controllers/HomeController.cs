8cbf2998ab1159cf1b1ca3db58923741a732bf06
verilye <connor.j.logan@outlook.com>
2022-04-30 12:44:22 +1000

    Login validation is completed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MusicSubscription.Models;
+using Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
 
 namespace MusicSubscription.Controllers;
 
@@ -8,9 +13,12 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IAmazonDynamoDB _dynamoDb;
+
+    public HomeController(IAmazonDynamoDB dynamoDB, ILogger<HomeController> logger)
     {
         _logger = logger;
+        _dynamoDb = dynamoDB;
     }
     
     public IActionResult Index()
@@ -19,10 +27,34 @@ public class HomeController : Controller
     }
 
     [HttpPost]
-    public IActionResult Index(string email)
+    public async Task<IActionResult> IndexAsync(string email, string password)
     {
-        return Content($"Hello {email}");
+        //Query for unique ID, scan for attributes
+
+        string tableName = "login";
+        Table ThreadTable = Table.LoadTable(_dynamoDb, tableName);
+
+        ScanFilter scanFilter = new ScanFilter();
+        scanFilter.AddCondition("email", ScanOperator.Equal, email);
+        scanFilter.AddCondition("password", ScanOperator.Equal, password);
+        Search search = ThreadTable.Scan(scanFilter);
+
+        var result = search.Count;
+
+        if(result == 1){
+
+            return View();
+
+        }else{
+
+            ViewData["error"] = "email or password is invalid";
+            return View();
+
+        }
+
+        
     }
+        
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
